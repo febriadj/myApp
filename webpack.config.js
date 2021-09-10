8c9bc83c94ac5619567647f1e639b3577a61e458
febriadj <iamfebriadji@gmail.com>
2021-09-10 22:29:04 +0700

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client', 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|s[ac]ss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'url-loader'],
+      },
+      {
+        test: /\.(md|png|jpe?g|gif)$/,
+        exclude: /node_modules/,
+        use: ['file-loader', 'url-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'client', 'build'),
+    },
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+}
