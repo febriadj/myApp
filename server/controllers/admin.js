fdce281b006e879250fd5fb413054077d8e21261
febriadj <iamfebriadji@gmail.com>
2021-09-11 00:47:59 +0700

    Update admin controller

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,17 +1,26 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const AdminModel = require('../models/admin.model');
+
+exports.AdminSession = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    // Mengirim data session ke client
+    data: req.session,
+  });
+}
 
 exports.AdminRegister = async (req, res) => {
   try {
     const Admin = new AdminModel({
       username: req.body.username,
       email: req.body.email,
-      // Enkripsi password
+      // Enkripsi Password
       password: bcrypt.hashSync(req.body.password, 8),
     });
 
     const data = await Admin.save();
-    // Mengembalikan data register ke client
+    // Mengembalikan data register ke Client
     res.status(200).json({
       status: 'success', data,
     });
@@ -34,13 +43,44 @@ exports.AdminLogin = async (req, res) => {
       ],
     });
 
-    // Compare Password
+    // Kondisi jika username atau email user tidak ditemukan
+    if (!data) {
+      const newErr = {
+        message: 'user not found',
+      }
+      throw newErr;
+    }
+
+    // Membandingkan Password
     if (!bcrypt.compareSync(req.body.password, data.password)) {
       const newErr = {
         message: 'password does not match',
       }
       throw newErr;
     }
+
+    // Membuat Token
+    const token = await jwt.sign({ id: data._id }, process.env.PRIVATE_KEY);
+    // Menyimpan Token kedalam Session
+    req.session.admin = token;
+
+    res.status(200).json({
+      status: 'success', data,
+    });
+  }
+  catch (error0) {
+    const { message } = error0;
+
+    res.status(400).json({
+      status: 'failed', message,
+    });
+  }
+}
+
+exports.AdminDelete = async (req, res) => {
+  try {
+    // Menghapus dokumen berdasarkan id
+    const data = await AdminModel.findOneAndDelete({ _id: req.query.id });
 
     res.status(200).json({
       status: 'success', data,
