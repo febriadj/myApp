1785896f930b77a687d45c19e679e6c71d06a483
febriadj <iamfebriadji@gmail.com>
2021-09-11 00:48:33 +0700

    Update Routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
-const { AdminRegister, AdminLogin } = require('../controllers/admin');
+const {
+  AdminSession,
+  AdminRegister,
+  AdminLogin,
+  AdminDelete,
+} = require('../controllers/admin');
 
-router.post('/register', AdminRegister);
-router.post('/login', AdminLogin);
+router.get('/admin/session', AdminSession);
+router.post('/admin/register', AdminRegister);
+router.post('/admin/login', AdminLogin);
+router.delete('/admin/delete', AdminDelete);
 
 module.exports = router;
