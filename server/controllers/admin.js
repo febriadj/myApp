0508c6b62d6efb927c15ece5ebb825763ab44608
febriadj <iamfebriadji@gmail.com>
2021-09-12 21:47:46 +0700

    Update admin controller

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 const AdminModel = require('../models/admin.model');
 
@@ -12,15 +14,64 @@ exports.AdminSession = (req, res) => {
 
 exports.AdminRegister = async (req, res) => {
   try {
+    // Membuat Kode acak untuk dijadikan kode Registrasi
+    const randomCode = Math.floor(1000 + Math.random() * 9000);
+    req.body.regisCode = randomCode;
+
+    // Inisialisasi Transporter Nodemailer
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'iamfebriadji@gmail.com',
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+
+    const emailContent = fs.readFileSync(`${__dirname}/../public/txt/emailverif.txt`, 'utf8');
+    // Send Mail
+    const data = await transporter.sendMail({
+      from: 'iamfebriadji@gmail.com',
+      to: req.body.email,
+      subject: 'Verify Your Email Address ~ Mr. Febx',
+      text: emailContent.replace(/%RegisCode%/g, req.body.regisCode),
+    });
+
+    req.session.regisData = req.body;
+    // Mengembalikan data register ke Client
+    res.status(200).json({
+      status: 'success', data,
+    });
+  }
+  catch (error0) {
+    res.status(200).json({
+      status: 'success', message: error0,
+    });
+  }
+}
+
+exports.AccAdminRegister = async (req, res) => {
+  try {
+    // Mengambil data Registrasi dari Session
+    const { username, email, password } = req.session.regisData;
+    const { clientRegisCode } = req.body;
+
+    if (req.session.regisData.regisCode !== clientRegisCode) {
+      const newErr = {
+        message: 'your registration code is wrong',
+      }
+      throw newErr;
+    }
+    // Jika berhasil, hapus properti regisData pada Object session
+    delete req.session.regisData;
+
     const Admin = new AdminModel({
-      username: req.body.username,
-      email: req.body.email,
+      username,
+      email,
       // Enkripsi Password
-      password: bcrypt.hashSync(req.body.password, 8),
+      password: bcrypt.hashSync(password, 8),
     });
 
     const data = await Admin.save();
-    // Mengembalikan data register ke Client
     res.status(200).json({
       status: 'success', data,
     });
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const {
   AdminSession,
   AdminRegister,
+  AccAdminRegister,
   AdminLogin,
   AdminDelete,
 } = require('../controllers/admin');
 
 router.get('/admin/session', AdminSession);
 router.post('/admin/register', AdminRegister);
+router.post('/admin/register/accept', AccAdminRegister);
 router.post('/admin/login', AdminLogin);
 router.delete('/admin/delete', AdminDelete);
 
