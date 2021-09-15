da3312dd2f0fa0bbce6b9e6de61e4df1c88400e2
febriadj <iamfebriadji@gmail.com>
2021-09-15 19:56:27 +0700

    Create article model

---
+++ server/models/article.model.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const ArticleSchema = new Schema({
+  author: {
+    type: Schema.Types.String,
+    unique: true,
+    required: true,
+  },
+  title: {
+    type: Schema.Types.String,
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  url: {
+    type: Schema.Types.String,
+    unique: true,
+    required: true,
+  },
+  filename: {
+    type: Schema.Types.String,
+    unique: true,
+    required: true,
+  },
+  tags: {
+    type: Schema.Types.Array,
+  },
+}, {
+  timestamps: true,
+});
+
+const ArticleModel = model('articles', ArticleSchema);
+module.exports = ArticleModel;
