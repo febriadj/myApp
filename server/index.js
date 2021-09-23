96c25f5f2af0b6f4aeadf49da2b982424228933b
febriadj <iamfebriadji@gmail.com>
2021-09-23 15:23:47 +0700

    Update server side app

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const ngrokConnection = require('./ngrok');
 
 const port = process.env.PORT || 8000;
 
-ngrokConnection(port);
+if (process.env.NODE_ENV === 'development') {
+  ngrokConnection(port);
+}
 
 app.listen(port, () => {
   console.log(`[${port}] Server Listening...`);
